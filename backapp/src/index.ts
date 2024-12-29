35c4cd24d1390f81741f5dc13b069647511e383a
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2024-12-29 23:03:36 +0100

    settings trpc

--- backapp/src/index.ts
+++ backapp/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { trpcRouter } from './trpc';
 
 const expressApp = express();
 
 expressApp.get('/ping', (_, res) => {
   res.send('pong');
 });
+
+expressApp.use('/trpc', trpcExpress.createExpressMiddleware({ router: trpcRouter }));
 
 expressApp.listen(3000, () => {
   console.log('Server started at http://localhost:3000');
