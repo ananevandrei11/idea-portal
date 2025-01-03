d62c26c09791ff2287245ce2743e7e2466a872af
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2025-01-03 18:27:25 +0100

    add pass salt

--- webapp/src/pages/IdeaPage/index.tsx
+++ webapp/src/pages/IdeaPage/index.tsx
@@ -6,13 +6,17 @@ import { trpc } from '../../lib/trpc';
 
 export function IdeaPage() {
   const params = useParams<IdeaNickParams>();
-  const { data, isLoading, isFetching, isError } = trpc.getIdea.useQuery({ ideaNick: params.ideaNick || '' });
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdea.useQuery({ ideaNick: params.ideaNick || '' });
 
   if (isLoading || isFetching) {
     return <div>Loading...</div>;
   }
 
-  if (isError || !data || !data.idea) {
+  if (isError && error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (!data.idea) {
     return <div>Not found</div>;
   }
 
